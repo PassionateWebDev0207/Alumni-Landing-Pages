7206e746b6a1fdd1a13a6347b99c865b278692fd
kuch <p.developer0207@gmail.com>
2019-09-04 02:07:11 -0700

    update for page - not complete responsive

--- webpack.config.js
+++ webpack.config.js
@@ -46,6 +46,10 @@ module.exports = (env, options) => ({
         ]
       },
       {
+        test: /\.(woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      },
+      {
         test: /\.(png|jpg|gif|svg)$/,
         use: [
           {
