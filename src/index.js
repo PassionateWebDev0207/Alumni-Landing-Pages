3ecbf9c683abe7737f412dcba47ac11466ee91f1
kuch <p.developer0207@gmail.com>
2019-09-09 19:39:38 -0700

    update for grid

--- src/index.js
+++ src/index.js
@@ -27,4 +27,39 @@ $(document).ready(function() {
 	$("#carousel-prev").click(function(){
 		owl.trigger('prev.owl.carousel');
 	})
+
+	//navbar effect
+	window.onscroll = function() {
+		showStickyMenu();
+	};
+
+	let globalFlag = window.innerWidth > 991 ? true : false;
+	window.onresize = function() {
+		if (window.innerWidth > 991 && globalFlag == false) {
+			showStickyMenu();
+			globalFlag = true;
+		}
+		if (window.innerWidth < 992 && globalFlag == true) {
+			showStickyMenu();
+			globalFlag = false;
+		}
+	};
+
+	function showStickyMenu() {
+		let navbar = document.getElementById("originnav");
+		let sticky = 0;
+		if (navbar) {
+			sticky = navbar.offsetTop;
+		}
+		if (sticky == 0) {
+			sticky = 100;
+		}
+		if (window.pageYOffset >= sticky /*&& window.innerWidth > 991*/) {
+			document.getElementById("scrollnav").style.visibility = 'visible';
+			document.getElementById("scrollnav").style.zIndex = '999999';
+		} else {
+			document.getElementById("scrollnav").style.visibility = 'hidden';
+			document.getElementById("scrollnav").style.zIndex = '-9';
+		}
+	}
 })
